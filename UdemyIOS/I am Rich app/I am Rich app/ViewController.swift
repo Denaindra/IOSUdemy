68c975d0792e01e734f9089b4fd9ca96687cfef0
mgdperera <mgdperera@gmail.com>
2018-11-17 09:34:59 +0530

    add image assets and project

---
+++ UdemyIOS/I am Rich app/I am Rich app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I am Rich app
+//
+//  Created by gayan perera on 11/17/18.
+//  Copyright © 2018 gayan perera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
