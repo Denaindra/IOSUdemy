663f8220df2972836cf9a0295fb0be3806f08def
mgdperera <mgdperera@gmail.com>
2018-11-19 09:59:02 +0530

    complete with core UI elements

--- UdemyIOS/Quizzes/Quizzes/Controller/ViewController.swift
+++ UdemyIOS/Quizzes/Quizzes/Controller/ViewController.swift
@@ -10,23 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var quizzText: UIView!
+    
+    
+    
+    @IBOutlet weak var quizzTex: UILabel!
     @IBOutlet weak var progressText: UILabel!
     @IBOutlet weak var scoreText: UILabel!
-    @IBOutlet weak var progressView: UIView!
+   // @IBOutlet weak var progressView: UIView!
+    @IBOutlet weak var progressWidth: NSLayoutConstraint!
     
-
+    private let questionList = QuestionBank();
+    private var pickedAnswer : Bool!
+    private var questionNumber : Int = 0
+    private var score : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       // UpdateUI()
+        NextQuestion()
     }
 
-    @IBAction func FasleClick(_ sender: UIButton) {
+    @IBAction func AnswerClick(_ sender: UIButton) {
+        if sender.tag == 1 {
+            pickedAnswer = true
+        }else if sender.tag == 0 {
+            pickedAnswer = false
+        }
+        CheckAnswer()
+        questionNumber += 1
+        NextQuestion();
+
+    }
+    
+    func UpdateUI() {
+        scoreText.text = "Score:\(score)"
+        progressText.text = "\(questionNumber + 1)/13"
+       progressWidth.constant = (view.frame.size.width / 13) * CGFloat(questionNumber + 1)
+       // progressView.backgroundColor = UIColor.green;
+        //progressView.frame.size.width 
+
         
     }
     
-    @IBAction func TrueClick(_ sender: UIButton) {
-        
+    func CheckAnswer() {
+        let curretAnswer = questionList.list[questionNumber].answer
+        if curretAnswer == pickedAnswer{
+            score += 1
+            print("correct")
+
+        }
+        else{
+            print("incorrect")
+        }
+        UpdateUI()
+
     }
+    
+    func NextQuestion() {
+
+        if questionNumber <= 12 {
+            quizzTex.text = questionList.list[questionNumber].questionText
+        }
+        else {
+            
+            let alert = UIAlertController(title: "Awsome", message: "Tour completed all questions", preferredStyle:.alert)
+            alert.addAction(UIAlertAction(title: "Restart", style:.default, handler: { action in
+                self.StartOver()
+            }))
+            present(alert,animated: true, completion: nil)
+        }
+
+    }
+    
+    func StartOver()
+    {
+        questionNumber = 0;
+        NextQuestion()
+    }
+    
+    
 }
 
--- UdemyIOS/Quizzes/Quizzes/Model/Questions.swift
+++ UdemyIOS/Quizzes/Quizzes/Model/Questions.swift
@@ -5,8 +5,8 @@ class Question{
     let questionText:String!
     let answer:Bool
     
-    init(text:String,correctAnszwer:Bool) {
+    init(text:String,correctAnswer:Bool) {
         questionText = text
-        answer = correctAnszwer
+        answer = correctAnswer
     }
 }
