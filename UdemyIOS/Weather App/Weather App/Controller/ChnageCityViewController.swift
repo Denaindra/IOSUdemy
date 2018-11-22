6590dd3fcdbad8767b98618a03148672c9f5b505
mgdperera <mgdperera@gmail.com>
2018-11-22 20:15:34 +0530

    complete UI view

---
+++ UdemyIOS/Weather App/Weather App/Controller/ChnageCityViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ChnageCityViewController.swift
+//  Weather App
+//
+//  Created by gayan perera on 11/22/18.
+//  Copyright © 2018 gayan perera. All rights reserved.
+//
+
+import UIKit
+
+class ChnageCityViewController: UIViewController {
+    
+    
+    @IBOutlet weak var chnageCityText: UITextField!
+    @IBOutlet weak var bakButton: UIButton!
+    @IBOutlet weak var getWeatherButton: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- UdemyIOS/Weather App/Weather App/Controller/ViewController.swift
+++ UdemyIOS/Weather App/Weather App/Controller/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tempretureLabel: UILabel!
+    @IBOutlet weak var weaherIcon: UIImageView!
+    @IBOutlet weak var citylabel: UILabel!
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
