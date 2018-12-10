532a6d00a2d6ced4018862305eaf8d60c11c537f
mgdperera <mgdperera@gmail.com>
2018-12-10 21:48:00 +0530

    user default

--- UdemyIOS/TodoeyApp/Todoey/Controller/ViewController.swift
+++ UdemyIOS/TodoeyApp/Todoey/Controller/ViewController.swift
@@ -9,19 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-
+    
     //outlets
     @IBOutlet weak var todoList: UITableView!
     
     //private properties
-    private let todoeyItems : [String] = ["Apple","mango","grapes","pinaapple",]
+    private var todoeyItems : [String] = ["Apple","mango","grapes","pinaapple",]
+    private let userDefaults = UserDefaults.standard
     //public properties
     
+    
+    //overrrde methods
     override func viewDidLoad() {
         super.viewDidLoad()
         self.todoList.register(UINib(nibName: "TodoTableViewCell", bundle: nil), forCellReuseIdentifier: "TodoTableViewCell")
-    }
+        
+        if  let list =  userDefaults.array(forKey: "todoeyItems") as? [String] {
+            todoeyItems = list
+        }
+            
+        
 
+    }
+    
+    //outlet methosds
+    @IBAction func AddnewItem(_ sender: Any) {
+        var newItem = UITextField()
+        let alert = UIAlertController(title: "Add New Item", message: " ", preferredStyle: UIAlertController.Style.alert)
+        let action = UIAlertAction(title: "Add", style: .default) { (alertAction) in
+            
+            self.todoeyItems.append(newItem.text!)
+            self.userDefaults.set(self.todoeyItems, forKey: "todoeyItems")
+            self.todoList.reloadData()
+            
+        }
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Enter Item name"
+            newItem = textField
+        }
+        alert.addAction(action)
+        present(alert,animated:true,completion:nil)
+    }
+    
+    
+    //bublic and private methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todoeyItems.count
     }
@@ -42,7 +74,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         }
     }
     
-
-
+    
+    
 }
 
